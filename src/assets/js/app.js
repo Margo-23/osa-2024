1fe251d6046515a0b57eaf43332de9c1b875be50
Margo-23 <issochka2017@gmail.com>
2024-01-16 18:52:52 +0200

    added header, off-canvas, footer

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,11 @@
-// import $ from 'zepto-modules';
+import $ from 'jquery';
+
+$(document).on('click', '[data-open]', function () {
+    const $target = $($(this).data('open'));
+    // $target.toggle(400);
+    $target.toggle();
+    $(this).toggleClass('open')
+    $target.toggleClass('open');
+});
+
+
